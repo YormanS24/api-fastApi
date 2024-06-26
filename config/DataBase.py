5c6d325189889efb563b92918b550e2b8773a098
YormanS24 <sanchezyorman0@gmail.com>
2024-06-26 09:56:25 -0500

    deploy_para_link_de_conexion

--- config/DataBase.py
+++ config/DataBase.py
@@ -2,9 +2,9 @@ from sqlalchemy import create_engine;
 from sqlalchemy.orm import sessionmaker;
 from sqlalchemy.ext.declarative import declarative_base;
 
-DATABASE_URL= "postgresql://neondb_owner:kKeybHoTl71M@ep-winter-heart-a5dyxtzc.us-east-2.aws.neon.tech/neondb?sslmode=require";
+DATABASE_URL = "postgresql://neondb_owner:kKeybHoTl71M@ep-winter-heart-a5dyxtzc.us-east-2.aws.neon.tech/neondb?sslmode=require&options=endpoint%3Dep-winter-heart-a5dyxtzc"
 
-engine = create_engine(DATABASE_URL,echo=True);
+engine = create_engine(DATABASE_URL);
 
 SessionLocal = sessionmaker(
     autocommit= False,
